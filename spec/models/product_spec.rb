a77424278130bbffc550b3f0af7544819d2d887a
James Huang <jamesy.huang@mosaic.com>
2013-11-29 18:36:56 -0500

    Implement product model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Product do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should ensure_length_of(:name).is_at_most(256) } 
+    it { should validate_uniqueness_of(:name) }
+    it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
+    it { should validate_presence_of(:sku_id) }
+    it { should validate_uniqueness_of(:sku_id) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:manufacturer) }
+  end
+
+end
