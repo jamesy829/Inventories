b1b91bfab217ed33b69906deaad96fd1d66bc811
Majorvin Tan <majorvin.tan@mosaic.com>
2014-03-12 15:25:57 -0400

    Rails update to 4.0.3

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,9 @@ module Inventories
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+
+    # http://stackoverflow.com/questions/20361428/rails-i18n-validation-deprecation-warning
+    config.i18n.enforce_available_locales = true
   end
 end
