3448a4eed6485ac1bc7d508c22222520dbc4e260
James Huang <jamesy.huang@hotmail.com>
2014-05-09 19:39:53 -0400

    Update production server IP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@192.168.1.30}
-role :web, %w{deploy@192.168.1.30}
-role :db,  %w{deploy@192.168.1.30}
+role :app, %w{deploy@192.168.1.135}
+role :web, %w{deploy@192.168.1.135}
+role :db,  %w{deploy@192.168.1.135}
 
 # Extended Server Syntax
 # ======================
@@ -14,7 +14,7 @@ role :db,  %w{deploy@192.168.1.30}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '192.168.1.30', user: 'deploy', roles: %w{web app db}, primary: true
+server '192.168.1.135', user: 'deploy', roles: %w{web app db}, primary: true
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
